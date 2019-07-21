ee1d59fbe1f8b21ae61c26144fe2f868807dfa12
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-07-21 17:08:09 +0200

    Simplify release tasks

---
+++ lib/teslamate/release.ex
@@ -0,0 +1,20 @@
+defmodule TeslaMate.Release do
+  @app :teslamate
+
+  def migrate do
+    for repo <- repos() |> IO.inspect() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    for r <- repos(), r == repo do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    end
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
