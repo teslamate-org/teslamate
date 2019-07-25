652a787da39793c62e3b7bc2e4f2aeb9890a4a81
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-07-25 16:56:31 +0200

    Install python in the builder stage to build on ARM

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM elixir:1.9-alpine AS builder
 
-RUN apk add --update --no-cache nodejs yarn git build-base && \
+RUN apk add --update --no-cache nodejs yarn git build-base python && \
     mix local.rebar --force && \
     mix local.hex --force
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,10 +4,6 @@ for app <- Application.spec(:teslamate, :applications) do
   {:ok, _} = Application.ensure_all_started(app)
 end
 
-# :ok = Application.ensure_all_started(TeslaMate.Repo)
-
 TeslaMate.Repo.start_link()
 
 ExUnit.start(assert_receive_timeout: 200)
-
-# Ecto.Adapters.SQL.Sandbox.mode(TeslaMate.Repo, :manual)
