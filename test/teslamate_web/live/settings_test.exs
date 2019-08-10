841a49b4b60bd2b7e1a914705ba04119bf61d939
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-10 16:50:43 +0200

    Add view for sleep settings

--- test/teslamate_web/live/settings_test.exs
+++ test/teslamate_web/live/settings_test.exs
@@ -12,10 +12,21 @@ defmodule TeslaMateWeb.SettingsLiveTest do
     assert {:ok, _view, html} = live(conn, "/settings")
 
     assert html =~
-             ~r(<select id="settings_unit_of_length" .+"><option value="km" selected>km</option><option value="mi">mi</option></select>)
+             ~r(<select id="settings_unit_of_length" .+><option value="km" selected>km</option><option value="mi">mi</option></select>)
 
     assert html =~
-             ~r(<select id="settings_unit_of_temperature" .+"><option value="C" selected>°C</option><option value="F">°F</option></select>)
+             ~r(<select id="settings_unit_of_temperature" .+><option value="C" selected>°C</option><option value="F">°F</option></select>)
+  end
+
+  @tag :signed_in
+  test "shows 12 and 15 minutes by default", %{conn: conn} do
+    assert {:ok, _view, html} = live(conn, "/settings")
+
+    assert html =~
+             ~r(<select id="settings_suspend_min" .+>.*<option value="12" selected>12 min</option>.*</select>)
+
+    assert html =~
+             ~r(<select id="settings_suspend_after_idle_min" .+>.*<option value="15" selected>15 min</option>.*</select>)
   end
 
   @tag :signed_in
@@ -23,15 +34,27 @@ defmodule TeslaMateWeb.SettingsLiveTest do
     assert {:ok, view, _html} = live(conn, "/settings")
 
     assert render_change(view, :change, %{settings: %{unit_of_length: :mi}}) =~
-             ~r(<select id="settings_unit_of_length" .+"><option value="km">km</option><option value="mi" selected>mi</option></select>)
+             ~r(<select id="settings_unit_of_length" .+><option value="km">km</option><option value="mi" selected>mi</option></select>)
 
     assert settings = Settings.get_settings!()
     assert settings.unit_of_length == :mi
 
     assert render_change(view, :change, %{settings: %{unit_of_temperature: :F}}) =~
-             ~r(<select id="settings_unit_of_temperature" .+"><option value="C">°C</option><option value="F" selected>°F</option></select>)
+             ~r(<select id="settings_unit_of_temperature" .+><option value="C">°C</option><option value="F" selected>°F</option></select>)
 
     assert settings = Settings.get_settings!()
     assert settings.unit_of_temperature == :F
+
+    assert render_change(view, :change, %{settings: %{suspend_min: 21}}) =~
+             ~r(<select id="settings_suspend_min" .+>.*<option value="21" selected>21 min</option>.*</select>)
+
+    assert settings = Settings.get_settings!()
+    assert settings.suspend_min == 21
+
+    assert render_change(view, :change, %{settings: %{suspend_after_idle_min: 30}}) =~
+             ~r(<select id="settings_suspend_after_idle_min" .+>.*<option value="30" selected>30 min</option>.*</select>)
+
+    assert settings = Settings.get_settings!()
+    assert settings.suspend_after_idle_min == 30
   end
 end
