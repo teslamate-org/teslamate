1c0d0641d57a2fe22c182885a99c19f8def753ae
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-10 13:56:40 +0200

    Render select dropdowns for units

--- test/teslamate/settings_test.exs
+++ test/teslamate/settings_test.exs
@@ -5,18 +5,20 @@ defmodule TeslaMate.SettingsTest do
   alias TeslaMate.Settings
 
   describe "settings" do
-    @update_attrs %{use_imperial_units: true}
-    @invalid_attrs %{use_imperial_units: nil}
+    @update_attrs %{unit_of_length: :mi, unit_of_temperature: :F}
+    @invalid_attrs %{unit_of_length: nil, unit_of_temperature: nil}
 
     test "get_settings!/0 returns the settings" do
       assert settings = Settings.get_settings!()
-      assert settings.use_imperial_units == false
+      assert settings.unit_of_length == :km
+      assert settings.unit_of_temperature == :C
     end
 
     test "update_settings/2 with valid data updates the settings" do
       settings = Settings.get_settings!()
       assert {:ok, %S{} = settings} = Settings.update_settings(settings, @update_attrs)
-      assert settings.use_imperial_units == true
+      assert settings.unit_of_length == :mi
+      assert settings.unit_of_temperature == :F
     end
 
     test "update_settings/2 with invalid data returns error changeset" do
--- test/teslamate_web/live/settings_test.exs
+++ test/teslamate_web/live/settings_test.exs
@@ -8,30 +8,30 @@ defmodule TeslaMateWeb.SettingsLiveTest do
   end
 
   @tag :signed_in
-  test "shows an unchecked checkbox by default", %{conn: conn} do
+  test "shows km and C by default", %{conn: conn} do
     assert {:ok, _view, html} = live(conn, "/settings")
 
-    assert html =~ """
-           <input id="settings_use_imperial_units" name="settings[use_imperial_units]" type="checkbox" value="true"> Use imperial units
-           """
+    assert html =~
+             ~r(<select id="settings_unit_of_length" .+"><option value="km" selected>km</option><option value="mi">mi</option></select>)
+
+    assert html =~
+             ~r(<select id="settings_unit_of_temperature" .+"><option value="C" selected>°C</option><option value="F">°F</option></select>)
   end
 
   @tag :signed_in
   test "reacts to change events", %{conn: conn} do
     assert {:ok, view, _html} = live(conn, "/settings")
 
-    assert render_change(view, :change, %{settings: %{use_imperial_units: true}}) =~ """
-           <input id="settings_use_imperial_units" name="settings[use_imperial_units]" type="checkbox" value="true" checked> Use imperial units
-           """
+    assert render_change(view, :change, %{settings: %{unit_of_length: :mi}}) =~
+             ~r(<select id="settings_unit_of_length" .+"><option value="km">km</option><option value="mi" selected>mi</option></select>)
 
     assert settings = Settings.get_settings!()
-    assert settings.use_imperial_units == true
+    assert settings.unit_of_length == :mi
 
-    assert render_change(view, :change, %{settings: %{use_imperial_units: false}}) =~ """
-           <input id="settings_use_imperial_units" name="settings[use_imperial_units]" type="checkbox" value="true"> Use imperial units
-           """
+    assert render_change(view, :change, %{settings: %{unit_of_temperature: :F}}) =~
+             ~r(<select id="settings_unit_of_temperature" .+"><option value="C">°C</option><option value="F" selected>°F</option></select>)
 
     assert settings = Settings.get_settings!()
-    assert settings.use_imperial_units == false
+    assert settings.unit_of_temperature == :F
   end
 end
