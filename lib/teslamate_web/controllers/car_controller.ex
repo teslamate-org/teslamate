43f9ad19f6476f76321583d0da515fcc33b604db
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-11 12:43:29 +0200

    Remove unused routes

--- lib/teslamate_web/controllers/car_controller.ex
+++ lib/teslamate_web/controllers/car_controller.ex
@@ -3,29 +3,9 @@ defmodule TeslaMateWeb.CarController do
 
   require Logger
 
-  alias TeslaMate.Log
-  alias TeslaMate.Log.Car
-  alias TeslaMate.Vehicles
+  alias TeslaMate.{Log, Vehicles}
 
   action_fallback TeslaMateWeb.FallbackController
-
-  def index(conn, _params) do
-    car = Log.list_cars()
-    render(conn, "index.json", car: car)
-  end
-
-  def show(conn, %{"id" => id}) do
-    car = Log.get_car!(id)
-    render(conn, "show.json", car: car)
-  end
-
-  def update(conn, %{"id" => id, "car" => car_params}) do
-    car = Log.get_car!(id)
-
-    with {:ok, %Car{} = car} <- Log.update_car(car, car_params) do
-      render(conn, "show.json", car: car)
-    end
-  end
 
   def suspend_logging(conn, %{"id" => id}) do
     car = Log.get_car!(id)
