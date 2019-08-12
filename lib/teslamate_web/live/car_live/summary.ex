bd575554d9f86915fc97c397b9720629b6c46230
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-12 18:47:19 +0200

    Suspend / resume logging via UI

--- lib/teslamate_web/live/car_live/summary.ex
+++ lib/teslamate_web/live/car_live/summary.ex
@@ -11,9 +11,12 @@ defmodule TeslaMateWeb.CarLive.Summary do
     if connected?(socket), do: Vehicles.subscribe(id)
 
     assigns = %{
+      id: id,
       summary: Vehicles.summary(id),
       settings: settings,
-      translate_state: &translate_state/1
+      translate_state: &translate_state/1,
+      error: nil,
+      error_timeout: nil
     }
 
     {:ok, assign(socket, assigns)}
@@ -25,6 +28,34 @@ defmodule TeslaMateWeb.CarLive.Summary do
   end
 
   @impl true
+  def handle_event("suspend_logging", _val, socket) do
+    cancel_timer(socket.assigns.error_timeout)
+
+    assigns =
+      case Vehicles.suspend_logging(socket.assigns.id) do
+        :ok ->
+          %{error: nil, error_timeout: nil}
+
+        {:error, reason} ->
+          %{
+            error: translate_error(reason),
+            error_timeout: Process.send_after(self(), :hide_error, 5_000)
+          }
+      end
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("resume_logging", _val, socket) do
+    :ok = Vehicles.resume_logging(socket.assigns.id)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:hide_error, socket) do
+    {:noreply, assign(socket, error: nil, error_timeout: nil)}
+  end
+
   def handle_info(summary, socket) do
     {:noreply, assign(socket, summary: summary)}
   end
@@ -38,4 +69,13 @@ defmodule TeslaMateWeb.CarLive.Summary do
   defp translate_state(:offline), do: gettext("offline")
   defp translate_state(:asleep), do: gettext("asleep")
   defp translate_state(:unavailable), do: gettext("unavailable")
+
+  defp translate_error(:unlocked), do: gettext("Car is unlocked")
+  defp translate_error(:sentry_mode), do: gettext("Sentry mode is enabled")
+  defp translate_error(:shift_state), do: gettext("Car is not parked")
+  defp translate_error(:preconditioning), do: gettext("Preconditioning")
+  defp translate_error(:user_present), do: gettext("User present")
+
+  defp cancel_timer(nil), do: :ok
+  defp cancel_timer(ref) when is_reference(ref), do: Process.cancel_timer(ref)
 end
