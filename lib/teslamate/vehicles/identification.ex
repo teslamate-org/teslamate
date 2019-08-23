699d7fb068b48d6ee7ca5dd1478127836dc6d84c
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-23 17:06:49 +0200

    Improve identification of performance models

--- lib/teslamate/vehicles/identification.ex
+++ lib/teslamate/vehicles/identification.ex
@@ -26,42 +26,45 @@ defmodule TeslaMate.Vehicles.Identification do
   defp which_model("MDL3"), do: "M3"
   defp which_model(______), do: false
 
-  defp is_performance("PX01"), do: true
   defp is_performance("P85D"), do: true
-  defp is_performance("PX6D"), do: true
-  defp is_performance("X024"), do: true
   defp is_performance("PBT8"), do: true
-  defp is_performance(______), do: nil
+  defp is_performance("PF01"), do: true
+  defp is_performance("PX01"), do: true
+  defp is_performance("PX6D"), do: true
+  defp is_performance("SPT31"), do: true
+  defp is_performance("MT304"), do: true
+  defp is_performance("X024"), do: true
+  defp is_performance(______), do: false
 
   defp which_battery("BT" <> _ = battery), do: battery
   defp which_battery(_), do: nil
 
-  defp get_efficiency("MS", "BTX5", _, true), do: {"S 75D", 0.186}
-  defp get_efficiency("MS", "BTX5", _, false), do: {"S 75", 0.185}
-  defp get_efficiency("MS", "BTX4", true, _), do: {"S P90D", 0.200}
-  defp get_efficiency("MS", "BTX4", false, _), do: {"S 90D", 0.189}
-  defp get_efficiency("MS", "BTX6", true, _), do: {"S P100D", 0.200}
-  defp get_efficiency("MS", "BTX6", false, _), do: {"S 100D", 0.189}
-  defp get_efficiency("MS", "BTX8", _, true), do: {"S 75D (85kWh)", 0.186}
-  defp get_efficiency("MS", "BTX8", _, false), do: {"S 75 (85kWh)", 0.185}
+  defp get_efficiency("MS", "BTX5", _perf, true), do: {"S 75D", 0.186}
+  defp get_efficiency("MS", "BTX5", _perf, false), do: {"S 75", 0.185}
+  defp get_efficiency("MS", "BTX4", true, _awd), do: {"S P90D", 0.200}
+  defp get_efficiency("MS", "BTX4", false, _awd), do: {"S 90D", 0.189}
+  defp get_efficiency("MS", "BTX6", true, _awd), do: {"S P100D", 0.200}
+  defp get_efficiency("MS", "BTX6", false, _awd), do: {"S 100D", 0.189}
+  defp get_efficiency("MS", "BTX8", _perf, true), do: {"S 75D (85kWh)", 0.186}
+  defp get_efficiency("MS", "BTX8", _perf, false), do: {"S 75 (85kWh)", 0.185}
   defp get_efficiency("MS", "BT85", true, true), do: {"S P85D", 0.201}
   defp get_efficiency("MS", "BT85", false, true), do: {"S 85D", 0.186}
   defp get_efficiency("MS", "BT85", true, false), do: {"S P85", 0.210}
   defp get_efficiency("MS", "BT85", false, false), do: {"S 85", 0.201}
-  defp get_efficiency("MS", "BT70", _, _), do: {"S 70 ?", 0.200}
-  defp get_efficiency("MS", "BT60", _, _), do: {"S 60 ?", 0.200}
-  defp get_efficiency("MS", _, _, _), do: {"S ???", 0.200}
+  defp get_efficiency("MS", "BT70", _perf, _awd), do: {"S 70 ?", 0.200}
+  defp get_efficiency("MS", "BT60", _perf, _awd), do: {"S 60 ?", 0.200}
+  defp get_efficiency("MS", ______, _perf, _awd), do: {"S ???", 0.200}
 
-  defp get_efficiency("MX", "BTX5", _, _), do: {"X 75D", 0.208}
-  defp get_efficiency("MX", "BTX4", false, _), do: {"X 90D", 0.208}
-  defp get_efficiency("MX", "BTX4", true, _), do: {"X P90D", 0.217}
-  defp get_efficiency("MX", "BTX6", false, _), do: {"X 100D", 0.208}
-  defp get_efficiency("MX", "BTX6", true, _), do: {"X P100D", 0.226}
-  defp get_efficiency("MX", _, _, _), do: {"X ???", 0.208}
+  defp get_efficiency("MX", "BTX5", _perf, _awd), do: {"X 75D", 0.208}
+  defp get_efficiency("MX", "BTX4", false, _awd), do: {"X 90D", 0.208}
+  defp get_efficiency("MX", "BTX4", true, _awd), do: {"X P90D", 0.217}
+  defp get_efficiency("MX", "BTX6", false, _awd), do: {"X 100D", 0.208}
+  defp get_efficiency("MX", "BTX6", true, _awd), do: {"X P100D", 0.226}
+  defp get_efficiency("MX", ______, _perf, _awd), do: {"X ???", 0.208}
 
-  defp get_efficiency("M3", "BT37", true, _), do: {"3P", 0.153}
-  defp get_efficiency("M3", "BT37", false, _), do: {"3", 0.153}
-  defp get_efficiency("M3", _, _, _), do: {"3 ???", 0.153}
+  defp get_efficiency("M3", "BT37", false, _awd), do: {"3", 0.153}
+  defp get_efficiency("M3", "BT37", true, _awd), do: {"3P", 0.153}
+  defp get_efficiency("M3", ______, _perf, _awd), do: {"3 ???", 0.153}
 
   defp get_efficiency(_, _, _, _), do: {"???", nil}
 end
