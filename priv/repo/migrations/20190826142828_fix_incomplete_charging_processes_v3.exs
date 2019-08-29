77a580deca15774598564b5abd57e8f803dc5e75
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-08-29 14:06:29 +0200

    Disable data migration

--- priv/repo/migrations/20190826142828_fix_incomplete_charging_processes_v3.exs
+++ priv/repo/migrations/20190826142828_fix_incomplete_charging_processes_v3.exs
@@ -1,32 +1,33 @@
 defmodule TeslaMate.Repo.Migrations.FixIncompleteChargingProcessesV3 do
   use Ecto.Migration
 
-  alias TeslaMate.Log.{ChargingProcess, Charge}
-  alias TeslaMate.Repo
+  # alias TeslaMate.Log.{ChargingProcess, Charge}
+  # alias TeslaMate.Repo
 
-  import Ecto.Query
+  # import Ecto.Query
 
   def up do
-    incomplete_charging_processes =
-      ChargingProcess
-      |> select([c], c.id)
-      |> where([c], is_nil(c.end_date))
-      |> Repo.all()
+    # incomplete_charging_processes =
+    #   ChargingProcess
+    #   |> select([c], c.id)
+    #   |> where([c], is_nil(c.end_date))
+    #   |> Repo.all()
 
-    for id <- incomplete_charging_processes do
-      {:ok, cproc} = TeslaMate.Log.complete_charging_process(id)
+    # for id <- incomplete_charging_processes do
+    #   {:ok, cproc} = TeslaMate.Log.complete_charging_process(id)
 
-      %{end_date: end_date} =
-        Charge
-        |> select([c], %{end_date: max(c.date)})
-        |> where(charging_process_id: ^id)
-        |> Repo.one()
+    #   %{end_date: end_date} =
+    #     Charge
+    #     |> select([c], %{end_date: max(c.date)})
+    #     |> where(charging_process_id: ^id)
+    #     |> Repo.one()
 
-      {:ok, _} =
-        cproc
-        |> ChargingProcess.changeset(%{end_date: end_date})
-        |> Repo.update()
-    end
+    #   {:ok, _} =
+    #     cproc
+    #     |> ChargingProcess.changeset(%{end_date: end_date})
+    #     |> Repo.update()
+    # end
+    :ok
   end
 
   def down do
