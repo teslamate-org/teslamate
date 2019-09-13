21ee80eb3c1f0cca3c401e4ceb5c2bb771d7151d
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-09-13 13:52:41 +0200

    Format js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  for (const el of document.querySelectorAll('.navbar-burger')) {
-    el.addEventListener('click', () => {
-      const $target = document.getElementById(el.dataset.target)
-      $target.classList.toggle('is-active')
-      el.classList.toggle('is-active')
-    })
+document.addEventListener("DOMContentLoaded", () => {
+  for (const el of document.querySelectorAll(".navbar-burger")) {
+    el.addEventListener("click", () => {
+      const $target = document.getElementById(el.dataset.target);
+      $target.classList.toggle("is-active");
+      el.classList.toggle("is-active");
+    });
   }
-})
+});
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -6,9 +6,9 @@
 //
 // Pass the token on params as below. Or remove it
 // from the params if you are not using authentication.
-import {Socket} from "phoenix"
+import { Socket } from "phoenix";
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -52,12 +52,17 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 //     end
 //
 // Finally, connect to the socket:
-socket.connect()
+socket.connect();
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+let channel = socket.channel("topic:subtopic", {});
+channel
+  .join()
+  .receive("ok", resp => {
+    console.log("Joined successfully", resp);
+  })
+  .receive("error", resp => {
+    console.log("Unable to join", resp);
+  });
 
-export default socket
+export default socket;
