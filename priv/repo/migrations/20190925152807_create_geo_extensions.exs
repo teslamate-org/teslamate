c805342bf256483ee3586cc09b8d35ff2fc0553b
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-09-30 00:42:52 +0200

    Don't throw an error if extensions already exist

--- priv/repo/migrations/20190925152807_create_geo_extensions.exs
+++ priv/repo/migrations/20190925152807_create_geo_extensions.exs
@@ -2,8 +2,8 @@ defmodule TeslaMate.Repo.Migrations.CreateGeoExtensions do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION cube", "DROP EXTENSION cube")
-    execute("CREATE EXTENSION earthdistance", "DROP EXTENSION earthdistance")
+    execute("CREATE EXTENSION IF NOT EXISTS cube", "DROP EXTENSION cube")
+    execute("CREATE EXTENSION IF NOT EXISTS earthdistance", "DROP EXTENSION earthdistance")
     create(index(:geofences, ["(earth_box(ll_to_earth(latitude, longitude), radius))"]))
   end
 end
