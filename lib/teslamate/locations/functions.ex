d5691e2cea16400e0de8e8b1f46ce7455a01aafd
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-10-08 18:04:41 +0200

    Add GIST index based on ll_to_earth

--- lib/teslamate/locations/functions.ex
+++ lib/teslamate/locations/functions.ex
@@ -6,12 +6,20 @@ defmodule TeslaMate.Locations.Functions do
   defmacro within_geofence?(position, geofence, :right) do
     quote do
       fragment(
-        "earth_box(ll_to_earth(?, ?), ?) @> ll_to_earth(?, ?)",
+        """
+        earth_box(ll_to_earth(?, ?), ?) @> ll_to_earth(?, ?) AND
+        earth_distance(ll_to_earth(?, ?), ll_to_earth(?, ?)) < ?
+        """,
         ^unquote(geofence).latitude,
         ^unquote(geofence).longitude,
         ^unquote(geofence).radius,
         unquote(position).latitude,
-        unquote(position).longitude
+        unquote(position).longitude,
+        ^unquote(geofence).latitude,
+        ^unquote(geofence).longitude,
+        unquote(position).latitude,
+        unquote(position).longitude,
+        ^unquote(geofence).radius
       )
     end
   end
@@ -19,12 +27,20 @@ defmodule TeslaMate.Locations.Functions do
   defmacro within_geofence?(position, geofence, :left) do
     quote do
       fragment(
-        "earth_box(ll_to_earth(?, ?), ?) @> ll_to_earth(?, ?)",
+        """
+        earth_box(ll_to_earth(?, ?), ?) @> ll_to_earth(?, ?) AND
+        earth_distance(ll_to_earth(?, ?), ll_to_earth(?, ?)) < ?
+        """,
         unquote(geofence).latitude,
         unquote(geofence).longitude,
         unquote(geofence).radius,
         ^unquote(position).latitude,
-        ^unquote(position).longitude
+        ^unquote(position).longitude,
+        unquote(geofence).latitude,
+        unquote(geofence).longitude,
+        ^unquote(position).latitude,
+        ^unquote(position).longitude,
+        unquote(geofence).radius
       )
     end
   end
