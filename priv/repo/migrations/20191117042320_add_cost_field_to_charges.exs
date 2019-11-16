2abe436a164b37b2d26a6cf8b7ee3fbd79c2fbb6
Nathan Gardiner <ngardiner@gmail.com>
2019-11-17 04:30:06 +1100

    Create 20191117042320_add_cost_field_to_charges.exs

---
+++ priv/repo/migrations/20191117042320_add_cost_field_to_charges.exs
@@ -0,0 +1,10 @@
+defmodule TeslaMate.Repo.Migrations.AddCostFieldToCharges do
+  use Ecto.Migration
+
+  def change do
+
+    alter table(:charges) do
+      add(:cost, :decimal(6,2))
+    end
+
+  end
