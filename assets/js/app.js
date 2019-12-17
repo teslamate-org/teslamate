207fa0c5259ee563c0abb73c31c2b79d65ba9356
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-12-17 17:55:42 +0100

    Update js dependencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,17 @@
 import css from "../css/app.scss";
 
-//// Import dependencies
-
 import "phoenix_html";
-
-//// Import local files
-
 import { Socket } from "phoenix";
 import LiveSocket from "phoenix_live_view";
+
 import * as hooks from "./hooks";
 
-const liveSocket = new LiveSocket("/live", Socket, {
+new LiveSocket("/live", Socket, {
   hooks,
   params: {
     baseUrl: window.location.origin,
     referrer: document.referrer
   }
-});
-liveSocket.connect();
+}).connect();
 
 import "./main";
