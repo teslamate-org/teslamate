3b7bb02c343c3a00a340dce4bdd70a2fef7edfc7
Christian Traber <ctraber@gmx.de>
2020-01-03 13:02:19 +0100

    format fixed

--- priv/repo/migrations/20200103073606_add_usable_battery_level.exs
+++ priv/repo/migrations/20200103073606_add_usable_battery_level.exs
@@ -2,10 +2,8 @@ defmodule TeslaMate.Repo.Migrations.AddUsableBatteryLevel do
   use Ecto.Migration
 
   def change do
-
     alter table(:positions) do
       add(:usable_battery_level, :integer)
     end
-
   end
 end
