43a9cb860f17b9cf3b8dc14736e1034dc629ae6e
Adrian <ayonix@users.noreply.github.com>
2020-10-08 10:33:56 +0200

    Display update version in the homescreen update tooltip (#976)
    * Make update_version of the API available
    
    * Add update version to the update tooltip
    
    * Fix formatting
    
    * Update translations to reflect the new tooltip
    
    * Fix car controller test
    
    * Add update_version to the mqtt documentation

--- test/teslamate/vehicles/vehicle/updating_test.exs
+++ test/teslamate/vehicles/vehicle/updating_test.exs
@@ -8,7 +8,7 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
 
     events = [
       {:ok, online_event()},
-      {:ok, update_event(now_ts - 1, "available", nil)},
+      {:ok, update_event(now_ts - 1, "available", nil, update_version: "2019.8.5 3aaa23d")},
       {:ok, update_event(now_ts, "installing", "2019.8.4 530d1d3")},
       {:ok, update_event(now_ts + 1, "installing", "2019.8.4 530d1d3")},
       {:ok, update_event(now_ts + 2, "installing", "2019.8.4 530d1d3")},
@@ -31,7 +31,12 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
 
     assert_receive {:pubsub,
                     {:broadcast, _server, _topic,
-                     %Summary{state: :online, since: s0, update_available: true}}}
+                     %Summary{
+                       state: :online,
+                       since: s0,
+                       update_available: true,
+                       update_version: "2019.8.5"
+                     }}}
 
     assert_receive {:start_update, ^car_id, [date: ^start_date]}
 
@@ -62,7 +67,7 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
 
     events = [
       {:ok, online_event()},
-      {:ok, update_event(now_ts - 1, "available", nil)},
+      {:ok, update_event(now_ts - 1, "available", nil, update_version: "2019.8.5 3aaa23d")},
       {:ok, update_event(now_ts, "installing", "2019.8.4 530d1d3")},
       {:ok, update_event(now_ts + 1, "foo", "2019.8.5 3aaa23d")},
       fn -> Process.sleep(10_000) end
@@ -79,7 +84,12 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
 
     assert_receive {:pubsub,
                     {:broadcast, _server, _topic,
-                     %Summary{state: :online, since: s0, update_available: true}}}
+                     %Summary{
+                       state: :online,
+                       since: s0,
+                       update_available: true,
+                       update_version: "2019.8.5"
+                     }}}
 
     assert_receive {:start_update, ^car_id, date: ^start_date}
     assert_receive {:pubsub, {:broadcast, _server, _topic, %Summary{state: :updating, since: s1}}}
@@ -102,8 +112,12 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
 
     events = [
       {:ok, online_event()},
-      {:ok, update_event(now_ts, "installing", "2019.8.4 530d1d3")},
-      {:ok, update_event(now_ts + 10, "available", "2019.8.4 530d1d3")},
+      {:ok,
+       update_event(now_ts, "installing", "2019.8.4 530d1d3", update_version: "2019.8.5 3aaa23d")},
+      {:ok,
+       update_event(now_ts + 10, "available", "2019.8.4 530d1d3",
+         update_version: "2019.8.5 3aaa23d"
+       )},
       fn -> Process.sleep(10_000) end
     ]
 
@@ -118,7 +132,8 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
     assert_receive {:start_update, ^car_id, date: ^date}
 
     assert_receive {:pubsub,
-                    {:broadcast, _server, _topic, %Summary{state: :updating, version: "2019.8.4"}}}
+                    {:broadcast, _server, _topic,
+                     %Summary{state: :updating, version: "2019.8.4", update_version: "2019.8.5"}}}
 
     assert_receive {:cancel_update, _upate_id}, 200
 
@@ -127,7 +142,8 @@ defmodule TeslaMate.Vehicles.Vehicle.UpdatingTest do
     assert_receive {:insert_position, ^car_id, %{}}
 
     assert_receive {:pubsub,
-                    {:broadcast, _server, _topic, %Summary{state: :online, version: "2019.8.4"}}}
+                    {:broadcast, _server, _topic,
+                     %Summary{state: :online, version: "2019.8.4", update_version: "2019.8.5"}}}
 
     refute_receive _
   end
