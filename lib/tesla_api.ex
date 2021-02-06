b1991f541e1d5731cc96424fd3e802ab4e0183bf
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-02-06 17:28:29 +0100

    Add support for v3 API tokens in China (#1300)
    * Add support for v3 API tokens in China
    
    * Add support for refreshing tokens in China
    
    * Log auth requests without debug output
    
    * Log failed requests to the Owner API

--- lib/tesla_api.ex
+++ lib/tesla_api.ex
@@ -3,8 +3,13 @@ defmodule TeslaApi do
 
   adapter Tesla.Adapter.Finch, name: TeslaMate.HTTP, receive_timeout: 35_000
 
+  plug Tesla.Middleware.Logger, debug: false, log_level: &log_level/1
   plug Tesla.Middleware.BaseUrl, "https://owner-api.teslamotors.com"
   plug Tesla.Middleware.Headers, [{"user-agent", "github.com/adriankumpf/teslamate"}]
-  plug TeslaApi.Auth.Middleware
   plug Tesla.Middleware.JSON
+  plug TeslaApi.Middleware.TokenAuth
+
+  defp log_level(%Tesla.Env{} = env) when env.status >= 500, do: :warn
+  defp log_level(%Tesla.Env{} = env) when env.status >= 400, do: :info
+  defp log_level(%Tesla.Env{}), do: :debug
 end
