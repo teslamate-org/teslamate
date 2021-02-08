e82bf1747be2d9f40eaed13eaff7df36dfc345ef
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-02-08 18:57:47 +0100

    Don't purge debug log statements
    Related to #1289

--- lib/tesla_api.ex
+++ lib/tesla_api.ex
@@ -3,11 +3,11 @@ defmodule TeslaApi do
 
   adapter Tesla.Adapter.Finch, name: TeslaMate.HTTP, receive_timeout: 35_000
 
-  plug Tesla.Middleware.Logger, debug: false, log_level: &log_level/1
   plug Tesla.Middleware.BaseUrl, "https://owner-api.teslamotors.com"
   plug Tesla.Middleware.Headers, [{"user-agent", "github.com/adriankumpf/teslamate"}]
   plug Tesla.Middleware.JSON
   plug TeslaApi.Middleware.TokenAuth
+  plug Tesla.Middleware.Logger, debug: false, log_level: &log_level/1
 
   defp log_level(%Tesla.Env{} = env) when env.status >= 500, do: :warn
   defp log_level(%Tesla.Env{} = env) when env.status >= 400, do: :info
