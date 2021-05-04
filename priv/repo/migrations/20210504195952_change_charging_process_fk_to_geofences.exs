90d6d205c082dde39a0f53a95ade49799d117493
emellodew <ed@dalestech.com>
2021-05-04 21:54:45 +0100

    Create 20210504195952_change_charging_process_fk_to_geofences.exs
    Stop geofences being deleted if used in charging history

---
+++ priv/repo/migrations/20210504195952_change_charging_process_fk_to_geofences.exs
@@ -0,0 +1,10 @@
+defmodule TeslaMate.Repo.Migrations.ChangeChargingProcessFkToGeofences do
+  use Ecto.Migration
+
+  def change do
+    alter table(:charging_processes) do
+      modify :geofence_id, references(:geofences, column: :id, type: :integer, on_delete: :restrict ), null: true,
+         from: references(:geofences, column: :id, type: :integer, on_delete: :nilify_all ), null: true
+    end
+  end
+end
