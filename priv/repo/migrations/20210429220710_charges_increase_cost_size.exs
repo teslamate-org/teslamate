ec9fcb4317a53f686d6fb23d27eee963e7454661
emellodew <ed@dalestech.com>
2021-05-14 17:18:31 +0100

    Update 20210429220710_charges_increase_cost_size.exs
    Fixes decimals issue on the charge_cost page.

--- priv/repo/migrations/20210429220710_charges_increase_cost_size.exs
+++ priv/repo/migrations/20210429220710_charges_increase_cost_size.exs
@@ -3,13 +3,13 @@ defmodule TeslaMate.Repo.Migrations.ChargesIncreaseCostSize do
 
   def up do
     alter table(:charging_processes) do
-      modify(:cost, :decimal, precision: 8, scale: 2, from: :decimal, precision: 6, scale: 2)
+      modify(:cost, :decimal, precision: 10, scale: 4, from: :decimal, precision: 6, scale: 2)
     end
   end
 
   def down do
     alter table(:charging_processes) do
-      modify(:cost, :decimal, precision: 6, scale: 2, from: :decimal, precision: 8, scale: 2)
+      modify(:cost, :decimal, precision: 6, scale: 2, from: :decimal, precision: 10, scale: 4)
     end
   end
 end
