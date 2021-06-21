86e456ada58a88e092751ba502e2e7d16b597236
Ed-M72 <64371467+Ed-M72@users.noreply.github.com>
2021-06-21 18:44:30 +0100

    Update priv/repo/migrations/20210429164504_geofences_increase_cost_size.exs
    Co-authored-by: Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>

--- priv/repo/migrations/20210429164504_geofences_increase_cost_size.exs
+++ priv/repo/migrations/20210429164504_geofences_increase_cost_size.exs
@@ -1,7 +1,7 @@
 defmodule TeslaMate.Repo.Migrations.GeofencesIncreaseCostSize do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:geofences) do
       modify(:cost_per_unit, :decimal,
         precision: 7,
@@ -16,26 +16,6 @@ defmodule TeslaMate.Repo.Migrations.GeofencesIncreaseCostSize do
         scale: 2,
         from: :decimal,
         precision: 6,
-        scale: 2
-      )
-    end
-  end
-
-  def down do
-    alter table(:geofences) do
-      modify(:cost_per_unit, :decimal,
-        precision: 6,
-        scale: 4,
-        from: :decimal,
-        precision: 7,
-        scale: 4
-      )
-
-      modify(:session_fee, :decimal,
-        precision: 6,
-        scale: 2,
-        from: :decimal,
-        precision: 7,
         scale: 2
       )
     end
