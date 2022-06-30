85ab7537e030564bd278cf7b23fdf50efa939a8c
Nir <klinger.nir@gmail.com>
2022-06-30 20:43:42 +0300

    Added dark mode support for teslamate UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,8 @@ import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 
 import * as hooks from "./hooks";
+import "./main";
+import Darkmode from "darkmode-js";
 
 const csrfToken = document
   .querySelector("meta[name='csrf-token']")
@@ -26,4 +28,31 @@ liveSocket.connect();
 // liveSocket.enableLatencySim(1000);
 // window.liveSocket = liveSocket;
 
-import "./main";
+
+const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+localStorage.setItem('theme', defaultDark ? 'dark' : 'light');
+
+window.onload = function toggleDarkMode(_state) {
+  const darkmode = new Darkmode();
+
+  if (localStorage.getItem("theme") === "light" && darkmode.isActivated()) {
+    darkmode.toggle();
+    window.location.reload()
+  } else if(localStorage.getItem("theme") === "dark" && !darkmode.isActivated()){
+    darkmode.toggle();
+    window.location.reload()
+  }
+}
+let element = window.matchMedia('(prefers-color-scheme: dark)');
+if (element) {
+  element.addEventListener("change", evt => {
+    const darkmode = new Darkmode();
+    if (evt.matches && !darkmode.isActivated()) {
+      darkmode.toggle()
+      window.location.reload()
+    } else {
+      darkmode.toggle();
+      window.location.reload()
+    }
+  })
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
+import Darkmode from "darkmode-js";
+
 document.querySelector(".navbar-burger").addEventListener("click", function () {
   const $target = document.getElementById(this.dataset.target);
   $target.classList.toggle("is-active");
   this.classList.toggle("is-active");
 });
+
+const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+localStorage.setItem('theme', defaultDark ? 'dark' : 'light');
+
+window.onload = function toggleDarkMode(_state) {
+  const darkmode = new Darkmode();
+
+  if (localStorage.getItem("theme") === "light" && darkmode.isActivated()) {
+    darkmode.toggle();
+  } else {
+    darkmode.toggle();
+  }
+}
+let element = window.matchMedia('(prefers-color-scheme: dark)');
+if (element) {
+  element.addEventListener("change", evt => {
+    const darkmode = new Darkmode();
+    if (evt.matches && !darkmode.isActivated()) {
+      darkmode.toggle()
+    } else {
+      darkmode.toggle();
+    }
+  })
+}
 
 for (const navDropdown of document.querySelectorAll(
   ".navbar-item.has-dropdown"
