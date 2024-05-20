263f207dc91bc50171878bc0c442adfe008823c7
jlestel <jlestel@gmail.com>
2024-05-20 13:11:13 +0200

    Dynamic endpoints and token to use official Tesla API (self-hosted or from third party provider) (#3866)
    * feat: endpoints by env
    
    * fix: typo
    
    * fix: useless env
    
    * fix: format
    
    * fix: distinct auth domain and url
    
    * format
    
    * fix: force issuer url if needed
    
    * feat: new streaming based on vin
    
    * fix refresh
    
    * revert
    
    * up
    
    * feat: no need for access token / refresh token if the TOKEN env var is present
    
    * feat: update login if token env var exists
    
    * feat: add ENV var to allow insecure wss
    
    * fix: remove TESLA_CN
    
    * fix(naming): TESLA_API_URL to TESLA_API_DOMAIN
    
    * feat: add an env var to allo invalid certs on WSS
    
    * doc: add API domains env vars description
    
    * fix: typo
    
    * feat: add env var to change log level
    
    * fix: APP_LOG_LEVEL
    
    * feat: add TOKEN documention and wording
    
    * fix: refacto insecure param
    
    * feat: naming and doc
    
    * fix: missing env var usage
    
    ---------
    
    Co-authored-by: Julien <julien@citio.digital>

--- lib/tesla_api/vehicle.ex
+++ lib/tesla_api/vehicle.ex
@@ -25,33 +25,38 @@ defmodule TeslaApi.Vehicle do
   def list(%Auth{} = auth) do
     endpoint_url =
       case Auth.region(auth) do
-        :chinese -> "https://owner-api.vn.cloud.tesla.cn"
-        _global -> "https://owner-api.teslamotors.com"
+        :chinese -> System.get_env("TESLA_API_HOST", "https://owner-api.vn.cloud.tesla.cn")
+        _global -> System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com")
       end
 
-    TeslaApi.get(endpoint_url <> "/api/1/products", opts: [access_token: auth.token])
+    TeslaApi.get(endpoint_url <> "/api/1/products" <> System.get_env("TOKEN", ""),
+      opts: [access_token: auth.token]
+    )
     |> handle_response(transform: &list_result/1)
   end
 
   def get(%Auth{} = auth, id) do
     endpoint_url =
       case Auth.region(auth) do
-        :chinese -> "https://owner-api.vn.cloud.tesla.cn"
-        _global -> "https://owner-api.teslamotors.com"
+        :chinese -> System.get_env("TESLA_API_HOST", "https://owner-api.vn.cloud.tesla.cn")
+        _global -> System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com")
       end
 
-    TeslaApi.get(endpoint_url <> "/api/1/vehicles/#{id}", opts: [access_token: auth.token])
+    TeslaApi.get(endpoint_url <> "/api/1/vehicles/#{id}" <> System.get_env("TOKEN", ""),
+      opts: [access_token: auth.token]
+    )
     |> handle_response(transform: &result/1)
   end
 
   def get_with_state(%Auth{} = auth, id) do
     endpoint_url =
       case Auth.region(auth) do
-        :chinese -> "https://owner-api.vn.cloud.tesla.cn"
-        _global -> "https://owner-api.teslamotors.com"
+        :chinese -> System.get_env("TESLA_API_HOST", "https://owner-api.vn.cloud.tesla.cn")
+        _global -> System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com")
       end
 
-    TeslaApi.get(endpoint_url <> "/api/1/vehicles/#{id}/vehicle_data",
+    TeslaApi.get(
+      endpoint_url <> "/api/1/vehicles/#{id}/vehicle_data" <> System.get_env("TOKEN", ""),
       query: [
         endpoints:
           "charge_state;climate_state;closures_state;drive_state;gui_settings;location_data;vehicle_config;vehicle_state;vehicle_data_combo"
