ba32b074003968a9c643ef9208a12fedc40dffef
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-10-27 08:00:26 +0100

    Revert "debug: floki paramter error"
    This reverts commit 61920267edf5186b593920bde7d87e8b30af55aa.

--- test/teslamate_web/live/settings_test.exs
+++ test/teslamate_web/live/settings_test.exs
@@ -209,7 +209,6 @@ defmodule TeslaMateWeb.SettingsLiveTest do
 
       TestHelper.eventually(fn ->
         html = render(view)
-        IO.inspect(html, label: "Rendered HTML")
 
         assert "There was a problem retrieving data from OpenStreetMap. Please try again later." =
                  html
