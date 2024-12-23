7b7ce2cf035e9147f743860a80976bc89bc2b39e
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-12-23 13:24:14 +0100

    feat: cancel an ongoing suspended state/trying to sleep and go back to online
    When power > 0.
    
    power > 0 is when e.g. the climate is on which could be scheduled preconditioning or starting preheat from the app.
    
    changes from @micves from #3262

--- lib/teslamate/vehicles/vehicle.ex
+++ lib/teslamate/vehicles/vehicle.ex
@@ -519,7 +519,20 @@ defmodule TeslaMate.Vehicles.Vehicle do
       %Stream.Data{shift_state: s, power: power}
       when s in [nil, "P"] and is_number(power) and power < 0 ->
         Logger.info("Suspended / Charging detected: #{power} kW", car_id: data.car.id)
-        {:next_state, prev_state, data, schedule_fetch(0, data)}
+
+        {:next_state, prev_state, %Data{data | last_used: DateTime.utc_now()},
+         schedule_fetch(0, data)}
+
+      %Stream.Data{shift_state: s, power: power}
+      when s in [nil, "P"] and is_number(power) and power > 0 ->
+        Logger.info("Suspended / Usage detected: #{power} kW", car_id: data.car.id)
+
+        # update power to be used in can_fall_asleep / try_to_suspend
+        vehicle = merge(data.last_response, stream_data, time: true)
+
+        {:next_state, prev_state,
+         %Data{data | last_response: vehicle, last_used: DateTime.utc_now()},
+         schedule_fetch(0, data)}
 
       %Stream.Data{} ->
         Logger.debug(inspect(stream_data), car_id: data.car.id)
