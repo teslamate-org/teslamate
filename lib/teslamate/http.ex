ceda0915e71e9b22cd6f25ab465e3c7758f75c2f
Julien <julien@citio.digital>
2025-03-31 10:19:43 +0200

    feat: customize HTTP limits when a user manage more than 10 vehicles

--- lib/teslamate/http.ex
+++ lib/teslamate/http.ex
@@ -1,12 +1,12 @@
 defmodule TeslaMate.HTTP do
   @pools %{
-    System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com") => [size: 10],
+    System.get_env("TESLA_API_HOST", "https://owner-api.teslamotors.com") => [size: System.get_env("TESLA_API_POOL_SIZE", 10)],
     "https://nominatim.openstreetmap.org" => [size: 3],
     "https://api.github.com" => [size: 1],
-    :default => [size: 5]
+    :default => [size: System.get_env("HTTP_POOL_SIZE", 5)]
   }
 
-  @pool_timeout 10_000
+  @pool_timeout System.get_env("HTTP_POOL_TIMEOUT", 10_000)
 
   def child_spec(_arg) do
     Finch.child_spec(name: __MODULE__, pools: @pools)
