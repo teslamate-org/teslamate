65ccbc52645aa92099d90b486cc784a8be6aafb5
Jeremy Fievet <jeremy.fievet@ext.leroymerlin.com>
2026-01-17 11:15:18 +0100

    translate fr teslamate

--- lib/teslamate_web/views/layout_view.ex
+++ lib/teslamate_web/views/layout_view.ex
@@ -4,7 +4,7 @@ defmodule TeslaMateWeb.LayoutView do
   import Phoenix.Component
   use PhoenixHTMLHelpers
 
-  dashboards =
+  dashboards_en =
     for dashboard_path <- Path.wildcard("grafana/dashboards/*.json") do
       @external_resource Path.relative_to_cwd(dashboard_path)
 
@@ -14,6 +14,26 @@ defmodule TeslaMateWeb.LayoutView do
       |> Map.take(["title", "uid"])
     end
 
-  @dashboards Enum.sort_by(dashboards, & &1["title"])
-  defp list_dashboards, do: @dashboards
+  dashboards_fr =
+    for dashboard_path <- Path.wildcard("grafana/dashboards/fr/*.json") do
+      @external_resource Path.relative_to_cwd(dashboard_path)
+
+      dashboard_path
+      |> File.read!()
+      |> Jason.decode!()
+      |> Map.take(["title", "uid"])
+    end
+
+  @dashboards_en Enum.sort_by(dashboards_en, & &1["title"])
+  @dashboards_fr Enum.sort_by(dashboards_fr, & &1["title"])
+
+  defp list_dashboards do
+    locale = Gettext.get_locale(TeslaMateWeb.Gettext) || ""
+
+    if String.starts_with?(locale, "fr") and @dashboards_fr != [] do
+      @dashboards_fr
+    else
+      @dashboards_en
+    end
+  end
 end
