63bd0210e05e797201338c65f8fba36c3118be61
Brian May <brian@linuxpenguins.xyz>
2026-04-14 09:04:56 +1000

    test: increase assert timeouts
    This was too fast, resulting in error messages
    appearing during the tests:
    
    09:04:21.725 [error] Postgrex.Protocol (#PID<0.461.0> (:proc_lib)) disconnected: ** (DBConnection.ConnectionError) client #PID<0.11120.0> exited
    
    Not 100% sure I understand, but increasing this limit makes the bad
    messages go away.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -12,5 +12,5 @@ end
 TeslaMate.Repo.start_link()
 Phoenix.PubSub.Supervisor.start_link(name: TeslaMate.PubSub)
 
-assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "300")
+assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "5000")
 ExUnit.start(assert_receive_timeout: assert_timeout)
