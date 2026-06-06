efc7b4d3f00e9ee8c8c00157660d09556e2a9eed
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2026-06-06 15:51:19 +0200

    build(nix): update mixFodDeps hash

--- nix/flake-modules/package.nix
+++ nix/flake-modules/package.nix
@@ -18,7 +18,7 @@
         TOP_SRC = src;
         pname = "${pname}-mix-deps";
         inherit src version;
-        hash = "sha256-FkbKFO+BNppDfuJJ6VZw4sPd3678OgMEPTANMer5hXA="; # if you change the mix deps, you need to update this hash
+        hash = "sha256-TcgmSqv2rGT9zP5AKBuTJzEpeWk9gH+fuxtXzBOWhEY="; # if you change the mix deps, you need to update this hash
         # hash = pkgs.lib.fakeHash;
       };
 
