0ee08114798668aff67fdb41f10619961ab2c93f
Matthias Wirtz <matthias.wirtz@hotmail.de>
2026-08-15 08:26:23 +0200

    test: stop the app in test_helper instead of relying on --no-start
    The helper used to depend on the caller passing --no-start. Stopping
    the app makes that starting state explicit and local, whoever launches
    the run. mix test and ElixirLS Test Explorer then get the same Repo +
    PubSub tree.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,12 @@
-# FIXME Workaround to apply the logger config when running the tests with
-# "--no-start". This looks like a bug in Elixir v1.11.0
+# ElixirLS Test Explorer starts the full app (it cannot pass --no-start).
+# Tear it down so tests get the same Repo + PubSub tree as `mix test`.
+case Application.stop(:teslamate) do
+  :ok -> :ok
+  {:error, {:not_started, :teslamate}} -> :ok
+end
+
+# `mix test --no-start` only loads config; Mix restarts Logger when it starts
+# the app. Bounce it here so config/test.exs (level: :warning) takes effect.
 Application.stop(:logger)
 Application.start(:logger)
 
@@ -9,8 +16,8 @@ for app <- Application.spec(:teslamate, :applications) do
   {:ok, _} = Application.ensure_all_started(app)
 end
 
-TeslaMate.Repo.start_link()
-Phoenix.PubSub.Supervisor.start_link(name: TeslaMate.PubSub)
+{:ok, _} = TeslaMate.Repo.start_link()
+{:ok, _} = Phoenix.PubSub.Supervisor.start_link(name: TeslaMate.PubSub)
 
 assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "300")
 ExUnit.start(assert_receive_timeout: assert_timeout)
