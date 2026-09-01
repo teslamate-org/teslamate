2f69ab31600baee987efd211bc9a187d80047efb
teslamate-actions <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-01 13:59:05 +0000

    build(deps): update Nix hashes

--- nix/flake-modules/package.nix
+++ nix/flake-modules/package.nix
@@ -20,7 +20,7 @@
         TOP_SRC = src;
         pname = "${pname}-mix-deps";
         inherit src version;
-        hash = "sha256-2Lf1SRnaxxH+zwYs3C1R8nt3bP6JUvd9R8Df32WHYW8="; # if you change the mix deps, you need to update this hash
+        hash = "sha256-GGuM0i8mETqKq83ukx0GAzgwkPbDkjmF+kkSqIwDqA0="; # if you change the mix deps, you need to update this hash
         # hash = pkgs.lib.fakeHash;
       };
 
