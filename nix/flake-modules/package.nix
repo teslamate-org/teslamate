bbab44bf636708dce72c29f89e25971491aa6399
teslamate-actions <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-20 22:17:23 +0000

    build(deps): update Nix hashes

--- nix/flake-modules/package.nix
+++ nix/flake-modules/package.nix
@@ -30,7 +30,7 @@
         inherit src version;
         # See src above for why the root is the repository, not elixir/.
         sourceRoot = "${srcName}/elixir";
-        hash = "sha256-Bfn/Y4ao8vMhlHh2NGNI+fRKd50CS+yNEYvx+GRI3BM="; # if you change the mix deps, you need to update this hash
+        hash = "sha256-uwO46VIhKzXnB8K0zAUxeoJcf2z91prVtTuYGobPgJk="; # if you change the mix deps, you need to update this hash
         # hash = pkgs.lib.fakeHash;
       };
 
